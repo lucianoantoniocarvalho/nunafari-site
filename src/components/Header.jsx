8a04befc502f55ff187c73269466693f3100950f
luciano-antonio-rowlands-carvalho <luciano.antonio.rowlands.carvalho@gmail.com>
2025-08-15 11:17:36 -0300

    Fixing logo's path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
     <header style={{ backgroundColor: "#21A51F", padding: "10px" }}>
-      <img 
-        src="src\assets\logo-nunafari-fundo-verde.png" 
-        alt="Nuna Fari logo" 
+      <img
+        src={`${import.meta.env.BASE_URL}assets/logo-nunafari-fundo-verde.png`}
+        alt="Nuna Fari logo"
         style={{ height: "50px", verticalAlign: "middle" }}
       />
       <span style={{ color: "#fff", marginLeft: "10px" }}>
