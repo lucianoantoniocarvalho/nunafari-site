9d2ea37c40c26ac5f15c2e6808c78aa0e5b2853f
luciano-antonio-rowlands-carvalho <luciano.antonio.rowlands.carvalho@gmail.com>
2025-08-14 22:29:53 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Bem-vindo à Nuna Fari</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header style={{ backgroundColor: "#21A51F", padding: "10px" }}>
+      <img 
+        src="src\assets\logo-nunafari-fundo-verde.png" 
+        alt="Nuna Fari logo" 
+        style={{ height: "50px", verticalAlign: "middle" }}
+      />
+      <span style={{ color: "#fff", marginLeft: "10px" }}>
+        Doing now. Doing the now.
+      </span>
+    </header>
+  );
+}
+
+export default Header;
