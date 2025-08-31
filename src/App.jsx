d6a94f368ee37da8340c734697c5998ceab84328
lucianoantoniocarvalho <lucianoantoniocarvalho@yahoo.com>
2025-08-31 12:16:49 -0300

    install Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <section>
-      <h1>Bem-vindo à Nuna Fari</h1>
-      <p>Conteúdo inicial da Home.</p>
+      <h1 className="bg-green-500 text-white text-4xl p-10">Bem-vindes à Nuna Fari!</h1>
+      <p className="text-red-600">Vamos comaçar a mexer agora.</p>
     </section>
   );
 }
